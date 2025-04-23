f89e0eb2821c0c855075d44b2e54642b6e2948dc
Lolookkk <152523414+Lolookkk@users.noreply.github.com>
2025-04-24 00:59:11 +0200

    Update Pokemon.h

--- projet C++ pokemon/Pokemon.h
+++ projet C++ pokemon/Pokemon.h
@@ -1,1 +1,19 @@
+#pragma once
+#ifndef ANIMAL_H
+#define ANIMAL_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+protected:
+    string nom;
+    int age;
+
+public:
+    Animal(string nom, int age);
+    void afficher();
+};
+
+#endif
